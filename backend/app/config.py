75ae1e92b2659acc0f5eb41ede08a10615bc16df
Kenny Leong <kennyleong2@gmail.com>
2023-03-07 15:30:48 -0500

    main branch files deleted.

--- backend/app/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Configuration:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    FLASK_ENV = os.environ.get('FLASK_ENV')
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
