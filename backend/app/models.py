8203e5d73112509ca2306030ea842316d0617150
Kenny Leong <kennyleong2@gmail.com>
2023-03-07 16:59:00 -0500

    server pic and user profile pic added to model.

--- backend/app/models.py
+++ backend/app/models.py
@@ -5,11 +5,13 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), nullable=False)
+    username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
+    profile_pic = db.Column(db.String(120))
     channels = db.relationship('Channel', backref='user', lazy=True)
     servers = db.relationship('Server', secondary='server_members', backref='members')
+
 
 class Channel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -33,6 +35,8 @@ class Server(db.Model):
     owner_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     channels = db.relationship('Channel', backref='server', lazy=True)
     members = db.relationship('User', secondary='server_members', backref='servers')
+    server_picture = db.Column(db.String(120))
+
 
 server_members = db.Table('server_members',
     db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
