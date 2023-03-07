dbcef0cdf999e8fccf26a4e3f72e94dcd9d7937f
Kenny Leong <kennyleong2@gmail.com>
2023-03-07 16:53:30 -0500

    model with server

--- backend/app/models.py
+++ backend/app/models.py
@@ -4,12 +4,18 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
     channels = db.relationship('Channel', backref='user', lazy=True)
+    servers = db.relationship('Server', secondary='server_members', backref='members')
 
 class Channel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -17,6 +23,7 @@ class Channel(db.Model):
     description = db.Column(db.String(120))
     created_by = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     messages = db.relationship('Message', backref='channel', lazy=True)
+    server_id = db.Column(db.Integer, db.ForeignKey('server.id'), nullable=False)
 
 class Message(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -24,3 +31,16 @@ class Message(db.Model):
     timestamp = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     channel_id = db.Column(db.Integer, db.ForeignKey('channel.id'), nullable=False)
+
+class Server(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    description = db.Column(db.String(120))
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    channels = db.relationship('Channel', backref='server', lazy=True)
+    members = db.relationship('User', secondary='server_members', backref='servers')
+
+server_members = db.Table('server_members',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
+    db.Column('server_id', db.Integer, db.ForeignKey('server.id'), primary_key=True)
+)
