f36d43c5d372b74b7a05e765b12a1b18942caec3
Aileen Kim <aileenkim96@gmail.com>
2023-03-10 18:29:44 -0500

    created seed data and updated routes

--- app/models/emoji.py
+++ app/models/emoji.py
@@ -11,9 +11,30 @@ class Emoji(db.Model):
     name = db.Column(db.String(40), nullable=False, unique=True)
     url = db.Column(db.String(100), nullable=False, unique=True)
 
+    # RELATIONSHIPS WITH FOREIGN KEYS
+
     def to_dict(self):
         return {
             'id': self.id,
             'name': self.name,
             'url': self.url
+        }
+
+
+class Reaction(db.Model):
+    __tablename__ = 'reactions'
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    #userId = db.Column()
+    #emojiId
+    #messageId
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            #'userId': self.name,
+            #'emojiId': self.url
         }
--- app/seeds/emojis.py
+++ app/seeds/emojis.py
@@ -2,6 +2,24 @@ from app.models import db, Emoji, environment, SCHEMA
 from sqlalchemy.sql import text
 
 def seed_emojis():
-    smile = Emoji(
+    smile = Emoji(name='smile', url='testingurl')
+    grinning = Emoji(name='grinning', url='testingurl')
+    joy = Emoji(name='joy', url='testingurl')
+    melting_face = Emoji(name='melting_face', url='testingurl')
+    innocent = Emoji(name='melting_face', url='testingurl')
+    # do more research ... how should i incorporate these emojis?
 
-    )
+    seed_emojis = [smile, grinning, joy, melting_face, innocent]
+
+    for emoji in seed_emojis:
+        db.session.add(emoji)
+    db.session.commit()
+
+
+def undo_emojis():
+    if environment == "production":
+        db.session.execute(f"TRUNCATE table {SCHEMA}.users RESTART IDENTITY CASCADE;")
+    else:
+        db.session.execute(text("DELETE FROM emojis"))
+
+    db.session.commit()
