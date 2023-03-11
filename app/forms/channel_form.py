bd2fc95e13d93a4ead664c4fa86eda998d65e226
Kenny Leong <kennyleong2@gmail.com>
2023-03-11 17:26:48 -0500

    form validation for channel created

--- app/forms/channel_form.py
+++ app/forms/channel_form.py
@@ -1,8 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, TextAreaField
 from wtforms.validators import DataRequired
-
-
 
 # for backend validation
 class ChannelForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    description = TextAreaField('Description')
--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,4 +1,5 @@
 from .db import db
 from .user import User
 from .message import Message
+from .channel import Channel
 from .db import environment, SCHEMA
