a8f111fd2fe5ff01db1ef76c26976e907093c7f7
Kenny Leong <kennyleong2@gmail.com>
2023-03-11 17:22:19 -0500

    channel model created.

---
+++ app/forms/channel_form.py
@@ -0,0 +1,8 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired
+
+
+
+# for backend validation
+class ChannelForm(FlaskForm):
---
+++ app/models/channel.py
@@ -0,0 +1,24 @@
+from .db import db, environment, SCHEMA
+from datetime import datetime
+
+
+class Channel(db.Model):
+    __tablename__ = 'channels'
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    description = db.Column(db.String(120))
+    messages = db.relationship('Message', backref='channel', lazy=True)
+    # server_id = db.Column(db.Integer, db.ForeignKey('server.id'), nullable=False)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description,
+            "messages": [message.to_dict() for message in self.messages],
+            # "serverId": self.serverId
+        }
