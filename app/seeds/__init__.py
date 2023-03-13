cb8a1fdafc5eb1d3e4303840e8bc467f207159e3
Kenny Leong <kennyleong2@gmail.com>
2023-03-13 12:10:54 -0400

    resolving merge conflict

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -18,11 +18,9 @@ def seed():
         # command, which will  truncate all tables prefixed with
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
-        undo_messages()
-        undo_users()
-    seed_users()
-    seed_channels()
-    seed_messages()
+        seed_users()
+        seed_channels()
+        seed_messages()
     # Add other seed functions here
 
 
