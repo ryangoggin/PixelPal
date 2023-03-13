24d9af55c51301aff801cdac34d1a63483945f6b
Aileen Kim <aileenkim96@gmail.com>
2023-03-13 12:28:17 -0400

    fixed seeders

--- app/seeds/friends.py
+++ app/seeds/friends.py
@@ -3,18 +3,29 @@ from sqlalchemy.sql import text
 from app.models.friend import Friend
 
 def seed_friends():
-    friend1 = Friend(userId=1, friendId=2)
-    friend2 = Friend(userId=1, friendId=3)
-    friend3 = Friend(userId=4, friendId=5)
-    friend4 = Friend(userId=4, friendId=6)
-    friend5 = Friend(userId=4, friendId=7)
+    friends = [
+    Friend(userId=1, friendId=2),
+    Friend(userId=2, friendId=1),
 
-    db.session.add_all([friend1, friend2, friend3, friend4, friend5])
+    Friend(userId=1, friendId=3),
+    Friend(userId=3, friendId=1),
+
+    Friend(userId=4, friendId=5),
+    Friend(userId=5, friendId=4),
+
+    Friend(userId=4, friendId=6),
+    Friend(userId=6, friendId=4),
+
+    Friend(userId=4, friendId=7),
+    Friend(userId=7, friendId=4)
+    ]
+
+    db.session.add_all(friends)
     db.session.commit()
 
 def undo_friends():
     if environment == "production":
-        db.session.execute(f"TRUNCATE table {SCHEMA}.users RESTART IDENTITY CASCADE;")
+        db.session.execute(f"TRUNCATE table {SCHEMA}.friends RESTART IDENTITY CASCADE;")
     else:
         db.session.execute(text("DELETE FROM friends"))
 
