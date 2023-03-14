08d1856d9e56b89fa043df0b7d171d4e1d8c9af0
Kenny Leong <kennyleong2@gmail.com>
2023-03-14 12:47:56 -0400

    moved files to navigation component

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -14,49 +14,9 @@ function App() {
   }, [dispatch]);
 
   return (
-    <div className="homepage">
-      {isLoaded && (
-        <div className="login-page">
-          <div className="login-box">
-            <div className="login-headings">
-              <span className="login-title">Welcome back!</span>
-              <span className="login-subtitle">We're so excited to see you again!</span>
-            </div>
-            <form className="login-form">
-              <div className="form-group">
-                <label className="form-label" htmlFor="emailOrPhone">
-                  EMAIL OR PHONE NUMBER
-                </label>
-                <br></br>
-                <input
-                  className="form-input"
-                  type="text"
-                  id="emailOrPhone"
-                  name="emailOrPhone"
-                />
-              </div>
-              <div className="form-group">
-                <label className="form-label" htmlFor="password">
-                  PASSWORD
-                </label>
-                <br></br>
-                <input
-                  className="form-input"
-                  type="password"
-                  id="password"
-                  name="password"
-                />
-              </div>
-              <button className="login-button" type="submit">Log In</button>
-              <div className="register-group">
-                <span className="label-register">Need an account?</span>
-                <span className="register">Register</span>
-              </div>
-            </form>
-          </div>
-        </div>
-      )}
-    </div>
+    <>
+      <Navigation isLoaded={isLoaded} />
+    </>
   );
 }
 
--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -8,16 +8,45 @@ function Navigation({ isLoaded }){
 	const sessionUser = useSelector(state => state.session.user);
 
 	return (
-		<ul>
-			<li>
-				<NavLink exact to="/">Home</NavLink>
-			</li>
-			{isLoaded && (
-				<li>
-					<ProfileButton user={sessionUser} />
-				</li>
-			)}
-		</ul>
+		<div className="login-page">
+			<div className="login-box">
+				<div className="login-headings">
+					<span className="login-title">Welcome back!</span>
+					<span className="login-subtitle">We're so excited to see you again!</span>
+				</div>
+				<form className="login-form">
+					<div className="form-group">
+						<label className="form-label" htmlFor="emailOrPhone">
+						EMAIL OR PHONE NUMBER
+						</label>
+						<br></br>
+						<input
+						className="form-input"
+						type="text"
+						id="emailOrPhone"
+						name="emailOrPhone"
+						/>
+					</div>
+					<div className="form-group">
+						<label className="form-label" htmlFor="password">
+						PASSWORD
+						</label>
+						<br></br>
+						<input
+						className="form-input"
+						type="password"
+						id="password"
+						name="password"
+						/>
+					</div>
+					<button className="login-button" type="submit">Log In</button>
+					<div className="register-group">
+						<span className="label-register">Need an account?</span>
+						<span className="register">Register</span>
+					</div>
+				</form>
+			</div>
+		</div>
 	);
 }
 
