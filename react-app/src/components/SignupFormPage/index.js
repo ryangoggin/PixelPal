60af86b64be26b164021e3238f0407dc7d814bdc
Kenny Leong <kennyleong2@gmail.com>
2023-03-15 14:57:52 -0400

    basic html signup done

--- react-app/src/components/SignupFormPage/index.js
+++ react-app/src/components/SignupFormPage/index.js
@@ -10,7 +10,9 @@ function SignupPage() {
   const [email, setEmail] = useState("");
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
+  const [month, setMonth] = useState("");
+  const [day, setDay] = useState("");
+  const [year, setYear] = useState("");
   const [errors, setErrors] = useState([]);
 
   if (sessionUser) return <Redirect to="/channels/@me" />;
@@ -19,21 +21,64 @@ function SignupPage() {
     e.preventDefault();
 
     // custom frontend validations
-    if (password === confirmPassword) {
-        const data = await dispatch(signUp(username, email, password));
-        if (data) {
-          setErrors(data)
-        }
-    } else {
-        setErrors(['Confirm Password field must be the same as the Password field']);
+
+
+  };
+
+  const generateOptions = (start, end) => {
+    const options = [];
+    for (let i = start; i <= end; i++) {
+      options.push(<option key={i} value={i}>{i}</option>);
     }
+    return options;
   };
 
   return (
-    <>
-      <h1>Register Page</h1>
-    </>
+    <div className="signup-container">
+      <h1 className="signup-header">Create an account</h1>
+      <form className="signup-form" onSubmit={handleSubmit}>
+        <label htmlFor="email" className="signup-input-label">EMAIL</label>
+        <input
+          type="email"
+          id="signup-email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="signup-input signup-email"
+        />
+        <label htmlFor="username" className="signup-input-label">USERNAME</label>
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          className="signup-input signup-username"
+        />
+        <label htmlFor="password" className="signup-input-label">PASSWORD</label>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="signup-input signup-password"
+        />
+        <div className="signup-dob">
+          <select id="month" value={month} onChange={(e) => setMonth(e.target.value)} className="signup-dob-select">
+            <option value="">Month</option>
+            {generateOptions(1, 12)}
+          </select>
+          <select id="day" value={day} onChange={(e) => setDay(e.target.value)} className="signup-dob-select">
+            <option value="">Day</option>
+            {generateOptions(1, 31)}
+          </select>
+          <select id="year" value={year} onChange={(e) => setYear(e.target.value)} className="signup-dob-select">
+            <option value="">Year</option>
+            {generateOptions(1900, new Date().getFullYear())}
+          </select>
+        </div>
+        <button type="submit" className="signup-submit-btn">Continue</button>
+      </form>
+      <span className="signup-already-account">Already have an account?</span>
+    </div>
   );
 }
+
 
 export default SignupPage;
