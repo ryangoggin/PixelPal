024ec120391ae6f2ef1703e6382caa46559150b4
Ryan Goggin <ryangoggin1@gmail.com>
2023-03-15 13:59:18 -0400

    added messages button for testing

--- react-app/src/components/FriendsList/index.js
+++ react-app/src/components/FriendsList/index.js
@@ -1,7 +1,18 @@
+import { Redirect } from "react-router"
+import { useHistory } from "react-router-dom"
+
 export default function FriendsList() {
+  const history = useHistory()
+
+  const redirect = async (e) => {
+    e.preventDefault()
+    history.push('/messages')
+  }
+
   return (
     <div>
       <h1> HELLO ! </h1>
+      <button onClick={redirect}>Messages</button>
     </div>
   )
 }
