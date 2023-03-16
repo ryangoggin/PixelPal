02fb21ca826908c31a9b36965a0285e94ae7e976
Aileen Kim <aileenkim96@gmail.com>
2023-03-15 20:02:03 -0400

    commented out unnecessary code (for now)

--- react-app/src/components/EmojisModal/index.js
+++ react-app/src/components/EmojisModal/index.js
@@ -1,27 +1,27 @@
-import React, { useEffect } from "react";
-import {useDispatch, useSelector} from 'react-redux'
-import { useModal } from "../../context/Modal";
-import OpenModalMenuItem from './OpenModalMenuItem';
-import { getAllEmojisThunk, createReactionThunk, loadOneEmojiThunk, allEmojis } from "../../store/emojis";
+// import React, { useEffect } from "react";
+// import {useDispatch, useSelector} from 'react-redux'
+// import { useModal } from "../../context/Modal";
+// import OpenModalMenuItem from './OpenModalMenuItem';
+// import { getAllEmojisThunk, createReactionThunk, loadOneEmojiThunk, allEmojis } from "../../store/emojis";
 
 import "./GetAllEmojis.css"
 
 export default function GetAllEmojis({messageId}) {
 
-  const dispatch = useDispatch()
-  const {closeModal} = useModal()
+  // const dispatch = useDispatch()
+  // const {closeModal} = useModal()
 
-  useEffect(() => {
-    dispatch(getAllEmojisThunk())
-  }, [dispatch])
+  // useEffect(() => {
+  //   dispatch(getAllEmojisThunk())
+  // }, [dispatch])
 
-  const emojis = useSelector(state => state.emoji.allEmojis)
-  const emojisArr = Object.values(emojis)
+  // const emojis = useSelector(state => state.emoji.allEmojis)
+  // const emojisArr = Object.values(emojis)
 
 
-  // console.log('emojis arr', emojisArr)
+  // // console.log('emojis arr', emojisArr)
 
-  const userId = useSelector(state => state.session.user?.id)
+  // const userId = useSelector(state => state.session.user?.id)
 
   // const createReaction = (emojiId, messageId, userId) => {
   // get create a reaction from the click
@@ -42,14 +42,14 @@ export default function GetAllEmojis({messageId}) {
 
   return (
     <div className='emoji-modal-container'>
-      {emojisArr.map(emoji => {
+      {/* {emojisArr.map(emoji => {
         return (<div className='emoji-modal-emoji'
                 value={emoji.id}
-                // onClick = {createReaction(emojiId=emoji.id, messageId, userId)}
+                onClick = {createReaction(emojiId=emoji.id, messageId, userId)}
                 >
                 {String.fromCodePoint(emoji.url)}
                 </div>)
-      })}
+      })} */}
     </div>
   )
 }
