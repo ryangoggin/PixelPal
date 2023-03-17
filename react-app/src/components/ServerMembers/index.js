3e18ac85ed53cfa02fa706109c81962c5d7657b0
Aileen Kim <aileenkim96@gmail.com>
2023-03-17 19:04:28 -0400

    server member now selecting properly

--- react-app/src/components/ServerMembers/index.js
+++ react-app/src/components/ServerMembers/index.js
@@ -1,22 +1,35 @@
-// import './ServerMembers.css'
+import { useSelector, useDispatch } from "react-redux";
+import { useParams } from "react-router-dom";
+import { getServer } from '../../store/server';
+import { useEffect } from 'react';
 
-import { useSelector } from "react-redux";
 
+import './ServerMembers.css'
 
 export default function ServerMembers() {
 
-  // let currServer = useSelector(state => state.server.currentServer);
+  // let currentServer = useSelector(state => state.server.currentServer);
+  const dispatch = useDispatch()
+  let { serverId } = useParams();
 
-  // let arr = Object.values(currServer)
-  // let membersArr = arr[0].members
-  // console.log('members arr from server members', membersArr)
+  let currentServer = useSelector(state => state.server.currentServer);
+  console.log(currentServer)
+
+  useEffect(() => {
+    dispatch(getServer(serverId))
+  }, [dispatch, serverId])
+
+  // const currentServerMembers = currentServer.members
+
+  // console.log('current server members ', currentServerMembers)
+
 
   // ADD LOGIC FOR SERVER OWNER
   return (
     <div>
       <div> SERVER MEMBERS</div>
       {/* <div className='servermembers-container'>
-        {membersArr.map((member) => {
+        {currentServerMembers.map((member) => {
           return (
             <div key={`servermember${member.id}`} >
               <div>
