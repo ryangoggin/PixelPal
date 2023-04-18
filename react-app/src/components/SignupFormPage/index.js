926522e12ca61feb25bc8d517aa99915cc9b7fff
Ryan Goggin <ryangoggin1@gmail.com>
2023-04-18 14:22:14 -0400

    fixed register background

--- react-app/src/components/SignupFormPage/index.js
+++ react-app/src/components/SignupFormPage/index.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, Redirect } from "react-router-dom";
 import { signUp } from "../../store/session";
+import wallpaper from "../../static/pixelpal-wallpaper.png";
 import './SignupForm.css';
 
 function SignupPage() {
@@ -30,57 +31,60 @@ function SignupPage() {
   };
 
   return (
-    <div className="signup-container">
-      <div className="signup-heading">
-        <span className="signup-header">Create an account</span>
-      </div>
-      <form className="signup-form" onSubmit={handleSubmit}>
-        <label htmlFor="email" id='email-label' className="signup-input-label">EMAIL</label>
-        <input
-          type="email"
-          id="signup-email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          className="signup-input signup-email"
-          required
-        />
-        <label htmlFor="username" id='username-label' className="signup-input-label">USERNAME</label>
-        <input
-          type="text"
-          id="signup-username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          className="signup-input signup-username"
-          required
-        />
-        <label htmlFor="password" id='password-label' className="signup-input-label">PASSWORD</label>
-        <input
-          type="password"
-          id="signup-password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          className="signup-input signup-password"
-          required
-        />
-        <div className="signup-dob-label">DATE OF BIRTH</div>
-        <div className="signup-dob">
-          <select id="month" value={month} onChange={(e) => setMonth(e.target.value)} className="signup-dob-select" required>
-            <option value="">Month</option>
-            {generateOptions(1, 12)}
-          </select>
-          <select id="day" value={day} onChange={(e) => setDay(e.target.value)} className="signup-dob-select" required>
-            <option value="">Day</option>
-            {generateOptions(1, 31)}
-          </select>
-          <select id="year" value={year} onChange={(e) => setYear(e.target.value)} className="signup-dob-select" required>
-            <option value="">Year</option>
-            {generateOptions(1940, new Date().getFullYear())}
-          </select>
+    <div className="signup-page-container">
+      <img className='wallpaper' src={wallpaper} alt="wallpaper" />
+      <div className="signup-container">
+        <div className="signup-heading">
+          <span className="signup-header">Create an account</span>
         </div>
-        <button type="submit" className="signup-submit-btn">Continue</button>
-      </form>
-      <Link to='/login' className="signup-already-account">Already have an account?</Link>
-      <span className="signup-tos">By registering, you agree to PixelPal's Terms of Service and Privacy Policy.</span>
+        <form className="signup-form" onSubmit={handleSubmit}>
+          <label htmlFor="email" id='email-label' className="signup-input-label">EMAIL</label>
+          <input
+            type="email"
+            id="signup-email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="signup-input signup-email"
+            required
+          />
+          <label htmlFor="username" id='username-label' className="signup-input-label">USERNAME</label>
+          <input
+            type="text"
+            id="signup-username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            className="signup-input signup-username"
+            required
+          />
+          <label htmlFor="password" id='password-label' className="signup-input-label">PASSWORD</label>
+          <input
+            type="password"
+            id="signup-password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="signup-input signup-password"
+            required
+          />
+          <div className="signup-dob-label">DATE OF BIRTH</div>
+          <div className="signup-dob">
+            <select id="month" value={month} onChange={(e) => setMonth(e.target.value)} className="signup-dob-select" required>
+              <option value="">Month</option>
+              {generateOptions(1, 12)}
+            </select>
+            <select id="day" value={day} onChange={(e) => setDay(e.target.value)} className="signup-dob-select" required>
+              <option value="">Day</option>
+              {generateOptions(1, 31)}
+            </select>
+            <select id="year" value={year} onChange={(e) => setYear(e.target.value)} className="signup-dob-select" required>
+              <option value="">Year</option>
+              {generateOptions(1940, new Date().getFullYear())}
+            </select>
+          </div>
+          <button type="submit" className="signup-submit-btn">Continue</button>
+        </form>
+        <Link to='/login' className="signup-already-account">Already have an account?</Link>
+        <span className="signup-tos">By registering, you agree to PixelPal's Terms of Service and Privacy Policy.</span>
+      </div>
     </div>
   );
 }
