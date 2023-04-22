9447290abe3c63552889188d22a00b7ce30e1771
Kenny Leong <kennyleong2@gmail.com>
2023-04-22 17:20:41 -0400

    unable to log a username with username without changing user model

--- app/forms/login_form.py
+++ app/forms/login_form.py
@@ -6,17 +6,18 @@ from app.models import User
 
 def user_exists(form, field):
     # Checking if user exists
-    email = field.data
-    user = User.query.filter(User.email == email).first()
+    email_or_username = form.data['email']
+    user = User.query.filter((User.email == email_or_username) | (User.username.like(f"%#{email_or_username}%"))).first()
+
     if not user:
-        raise ValidationError('Email provided not found.')
+        raise ValidationError('User not found.')
 
 
 def password_matches(form, field):
     # Checking if password matches
     password = field.data
-    email = form.data['email']
-    user = User.query.filter(User.email == email).first()
+    email_or_username = form.data['email']
+    user = User.query.filter((User.email == email_or_username) | (User.username.like(f"%#{email_or_username}%"))).first()
     if not user:
         raise ValidationError('No such user exists.')
     if not user.check_password(password):
