f7da63396412f9b1bdf15c0156aea8175a729522
Aileen Kim <aileenkim96@gmail.com>
2023-04-26 17:02:53 -0400

    updated socket.py

--- app/socket.py
+++ app/socket.py
@@ -5,8 +5,8 @@ import os
 # configure cors_allowed_origins
 if os.environ.get('FLASK_ENV') == 'production':
     origins = [
-        'http://pixelpal.onrender.com',
-        'https://pixelpal.onrender.com'
+        'http://pixel-pal.onrender.com',
+        'https://pixel-pal.onrender.com'
     ]
 else:
     origins = "*"
