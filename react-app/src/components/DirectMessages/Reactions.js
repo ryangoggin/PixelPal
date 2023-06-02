4de481791d4fd0955b7ef5faffb110e417e3e421
Aileen Kim <aileenkim96@gmail.com>
2023-06-02 15:18:38 -0400

    omg

--- react-app/src/components/DirectMessages/Reactions.js
+++ react-app/src/components/DirectMessages/Reactions.js
@@ -8,15 +8,16 @@ export default function DMReactions({reactions}) {
   return (
     <>
       {reactions.map((reaction) => {
+        return (
         <>
-        <div> {reaction.emoji.name} </div>
-        <div className='dm-msg-reaction-box'
+        <div className='reactions-each-container'
         key={`reaction${reaction.id}`}
         // onClick={+reaction.userId === +sessionUser?.id ? () => { deleteReaction(reaction.id, msg.id) } : () => { addReaction(reaction.emojiId, msg.id, sessionUser?.id) }}
         >
           {String.fromCodePoint(reaction.emoji.url)}
         </div>
       </>
+        )
       })}
     </>
 
